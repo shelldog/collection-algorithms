c6eef2b67f956e8c56cce1c7af9157ee3797cef3
ktran75 <khaquang.tran@dxc.com>
2022-04-06 16:14:07 +0700

    add 3sum.

---
+++ 3Sum/3sum.js
@@ -0,0 +1,42 @@
+let nums = [-1,0,0,1,2,-1,-4];
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+
+  let s = [];
+  
+  nums.sort((a, b) => {
+    return a - b;
+  })
+
+  // [-1, -1, 0, 0, 1, 2].
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let l = i + 1;
+    let r = nums.length - 1;
+    while (l < r) {
+      let t = nums[i] + nums[l] + nums[r];
+      if (t > 0) {
+        r--;
+      } else if (t < 0) {
+        l++;
+      } else {
+        s.push([nums[i], nums[l], nums[r]]);
+        l++;
+        while (nums[l] === nums[l - 1] && l < r) {
+          l++;
+        }
+      }
+    }
+  }
+
+  return s;
+};
+
+console.log(threeSum(nums));
